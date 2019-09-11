f45ff569c54b52ca67caccad7a6736860d79926a
marcosgabrielrodrigues <marcos.grm@yahoo.com>
2019-09-11 09:07:50 -0300

    Adicionei o componente marcosgabriel

---
+++ src/app/marcosgabriel/marcosgabriel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marcosgabriel',
+  templateUrl: './marcosgabriel.component.html',
+  styleUrls: ['./marcosgabriel.component.css']
+})
+export class MarcosgabrielComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
